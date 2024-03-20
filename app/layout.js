b20ccac39930e78e3e78d1874c68a11c5563b7d9
Dean Harland <dean.h@interstateteam.com>
2024-03-20 09:54:11 +0000

    minor

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
    title: "Interstate Screenboard",
-   description: "Interstate Office Screenboard",
+   description: "",
 };
 
 export default function RootLayout({ children }) {
