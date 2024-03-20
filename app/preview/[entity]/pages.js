6bef5f98538bc713f865221952be653fcf9f0bcc
Dean Harland <dean.h@interstateteam.com>
2024-03-20 10:35:31 +0000

    commit

---
+++ app/preview/[entity]/pages.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import styles from "./BoardContent.module.css";
+import Feature from "./Feature";
+
+export default function Entity() {
+   const [contentfulData, setContentfulData] = useState(null);
+
+   useEffect(() => {
+      retrieveContentfulData();
+   }, []);
+
+   const retrieveContentfulData = (_event) => {
+      fetch("/api/contentful", {
+         method: "GET",
+         headers: {
+            "content-type": "application/json",
+         },
+         cache: "no-store",
+      })
+         .then((res) => {
+            return res.json();
+         })
+         .then((jsonData) => {
+            return JSON.stringify(jsonData);
+         })
+         .then((jsonStr) => {
+            setContentfulData(JSON.parse(jsonStr).result);
+         });
+   };
+
+   function getModule() {
+      return <div>Enity Page!</div>;
+      /*
+      const item = contentfulData.board.items[index];
+      if (item) {
+         return <Feature data={item} onDispatch={getNext} />;
+      } else {
+         return null;
+      }
+      */
+   }
+
+   return <div className={styles.boardContent}>{contentfulData && getModule()}</div>;
+}
