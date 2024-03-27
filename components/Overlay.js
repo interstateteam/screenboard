399717c890f928d6ed989ea24860c8d8110b353a
Dean Harland <dean.h@interstateteam.com>
2024-03-27 17:39:44 +0000

    Weather widget removed

--- components/Overlay.js
+++ components/Overlay.js
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import { CSSTransition } from "react-transition-group";
 import localFont from "next/font/local";
 import styles from "./Overlay.module.css";
-import WeatherPanel from "./overlays/WeatherPanel";
+//import WeatherPanel from "./overlays/WeatherPanel";
 import NewsBanner from "./overlays/NewsBanner";
 
 const ReplicaLLWebB = localFont({ src: "../public/fonts/ReplicaLLWeb-Bold.woff2" });
@@ -18,9 +18,7 @@ export default function Overlay(props) {
    return (
       <div className={styles.overlay}>
          <CSSTransition in={props.show} nodeRef={panelRef} timeout={1000} classNames="weatherHide">
-            <div className={ReplicaLLWebB.className} ref={panelRef}>
-               <WeatherPanel />
-            </div>
+            <div className={ReplicaLLWebB.className} ref={panelRef}></div>
          </CSSTransition>
          {process.env.NODE_ENV === "development" && (
             <CSSTransition in={showNews} nodeRef={bannerRef} timeout={1000} classNames="newsHide">
